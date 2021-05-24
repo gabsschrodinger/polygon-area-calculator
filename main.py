11828de110d8856561350f36fcf6dc8b82172653
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-05-24 09:43:33 -0300

    Adding polygon area calculator app

---
+++ main.py
@@ -0,0 +1,50 @@
+import math
+
+class Rectangle:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+  
+  def set_width(self, width):
+    self.width = width
+
+  def set_height(self, height):
+    self.height = height
+
+  def get_area(self):
+    return self.width * self.height
+
+  def get_perimeter(self):
+    return 2 * self.width + 2 * self.height
+
+  def get_diagonal(self):
+    return (self.width ** 2 + self.height ** 2) ** .5
+
+  def get_picture(self):
+    if(self.width > 50 or self.height > 50): return "Too big for picture."
+    output = ""
+    for i in range(self.height):
+      for j in range(self.width):
+        output += "*"
+      output += "\n"
+    return output
+
+  def get_amount_inside(self, shape):
+    return math.floor(self.get_area() / shape.get_area())
+  
+  def __str__(self):
+    return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+
+
+
+class Square(Rectangle):
+  def __init__(self, side):
+    self.width = side
+    self.height = side
+
+  def set_side(self, side):
+    self.width = side
+    self.height = side
+
+  def __str__(self):
+    return "Square(side=" + str(self.width) + ")"
